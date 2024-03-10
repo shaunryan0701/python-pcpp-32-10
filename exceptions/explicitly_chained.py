a9a014183fb7dc92ff69ab4a723079fee33d1b24
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-03-10 12:54:32 +0000

    Added exception examples

---
+++ exceptions/explicitly_chained.py
@@ -0,0 +1,32 @@
+class RocketNotReadyError(Exception):
+    pass
+
+
+def personnel_check():
+    try:
+        print("\tThe captain's name is", crew[0])
+        print("\tThe pilot's name is", crew[1])
+        print("\tThe mechanic's name is", crew[2])
+        print("\tThe navigator's name is", crew[3])
+    except IndexError as e:
+        raise RocketNotReadyError('Crew is incomplete') from e
+
+
+def fuel_check():
+    try:
+        print('Fuel tank is full in {}%'.format(100 / 0))
+    except ZeroDivisionError as e:
+        raise RocketNotReadyError('Problem with fuel gauge') from e
+
+
+crew = ['John', 'Mary', 'Mike']
+fuel = 100
+check_list = [personnel_check, fuel_check]
+
+print('Final check procedure')
+
+for check in check_list:
+    try:
+        check()
+    except RocketNotReadyError as f:
+        print('RocketNotReady exception: "{}", caused by "{}"'.format(f, f.__cause__))
