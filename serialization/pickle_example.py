f36b6b39c1079bfc4d1d7659aa1ab8754353f74c
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-03-11 13:46:06 +0000

    Added serialization examples

---
+++ serialization/pickle_example.py
@@ -0,0 +1,27 @@
+import pickle
+
+a_dict = dict()
+a_dict['EUR'] = {'code':'Euro', 'symbol': '€'}
+a_dict['GBP'] = {'code':'Pounds sterling', 'symbol': '£'}
+a_dict['USD'] = {'code':'US dollar', 'symbol': '$'}
+a_dict['JPY'] = {'code':'Japanese yen', 'symbol': '¥'}
+
+a_list = ['a', 123, [10, 100, 1000]]
+
+with open('multidata.pkl', 'wb') as file_out:
+    pickle.dump(a_dict, file_out)
+    pickle.dump(a_list, file_out)
+
+
+file_out.close()
+
+with open('multidata.pkl', 'rb') as file_in:
+    data1 = pickle.load(file_in)
+    data2 = pickle.load(file_in)
+
+print(type(data1))
+print(data1)
+print(type(data2))
+print(data2)
+
+file_out.close()
---
+++ serialization/shelve_example.py
@@ -0,0 +1,17 @@
+import shelve
+
+shelve_name = 'first_shelve.shlv'
+
+my_shelve = shelve.open(shelve_name, flag='c')
+my_shelve['EUR'] = {'code':'Euro', 'symbol': '€'}
+my_shelve['GBP'] = {'code':'Pounds sterling', 'symbol': '£'}
+my_shelve['USD'] = {'code':'US dollar', 'symbol': '$'}
+my_shelve['JPY'] = {'code':'Japanese yen', 'symbol': '¥'}
+my_shelve.close()
+
+new_shelve = shelve.open(shelve_name)
+print(new_shelve['USD'])
+for key in new_shelve:
+    print (new_shelve[key])
+print(new_shelve.keys())
+new_shelve.close()
