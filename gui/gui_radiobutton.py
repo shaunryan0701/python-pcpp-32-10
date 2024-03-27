15fad67bc809b1bd79be224f65a3ee229af512a2
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-03-27 14:17:37 +0000

    Added more gui examples

---
+++ gui/gui_radiobutton.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def show():
+    messagebox.showinfo("", "radio_1=" + str(radio_1_var.get()) +
+                        ",radio_2=" + str(radio_2_var.get()))
+
+
+def command_1():
+    radio_2_var.set(radio_1_var.get())
+
+
+def command_2():
+    radio_1_var.set(radio_2_var.get())
+
+
+window = tk.Tk()
+button = tk.Button(window, text="Show", command=show)
+button.pack()
+radio_1_var = tk.IntVar()
+radio_1_1 = tk.Radiobutton(window, text="pizza", variable=radio_1_var, value=1, command=command_1)
+radio_1_1.select()
+radio_1_1.pack()
+radio_1_2 = tk.Radiobutton(window, text="clams", variable=radio_1_var, value=2, command=command_1)
+radio_1_2.pack()
+radio_2_var = tk.IntVar()
+radio_2_1 = tk.Radiobutton(window, text="FR", variable=radio_2_var, value=2, command=command_2)
+radio_2_1.pack()
+radio_2_2 = tk.Radiobutton(window, text="IT", variable=radio_2_var, value=1, command=command_2)
+radio_2_2.select()
+radio_2_2.pack()
+window.mainloop()
