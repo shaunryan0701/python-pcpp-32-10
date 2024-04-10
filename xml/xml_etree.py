7383b2cde3ff0ecb96744e29aaea620611494d47
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-04-10 09:15:04 +0100

    Added some xml examples

---
+++ xml/xml_etree.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree
+
+cars_for_sale = xml.etree.ElementTree.parse('cars.xml').getroot()
+print(cars_for_sale.tag)
+for car in cars_for_sale.findall('car'):
+    print('\t', car.tag)
+    for prop in car:
+        print('\t\t', prop.tag, end='')
+        if prop.tag == 'price':
+            print(prop.attrib, end='')
+    print(' =', prop.text)
