88b4f2cbce054e1063e8e7321536efc53deea95d
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2024-04-12 23:07:12 +0100

    Added some json examples

---
+++ json/json_dumps.py
@@ -0,0 +1,18 @@
+import json
+
+
+class Who:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+def encode_who(w):
+    if isinstance(w, Who):
+        return w.__dict__
+    else:
+        raise TypeError(w.__class__.__name__ + ' is not JSON serializable')
+
+
+some_man = Who('John Doe', 42)
+print(json.dumps(some_man, default=encode_who))
